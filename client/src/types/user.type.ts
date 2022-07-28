c25f27e4d14c3cf6469ae4d5cd2b358b7b5ea95b
eddybruv <edwinajong@gmail.com>
2022-07-28 17:54:27 +0100

    added the user type

---
+++ client/src/types/user.type.ts
@@ -0,0 +1,21 @@
+export interface IUser {
+  access: string;
+  avatar: string;
+  date_joined: string;
+  email: string;
+  first_name: string;
+  groups: [];
+  id: number;
+  isAdmin: boolean;
+  is_active: boolean;
+  is_staff: boolean;
+  is_superuser: boolean;
+  last_login: null;
+  last_name: string;
+  name: string;
+  password: string;
+  phone: string;
+  refresh: string;
+  token: string;
+  user_permissions: [];
+}
