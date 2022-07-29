e2ce89bd5838317960734c7a1f219dfcb375ff46
eddybruv <edwinajong@gmail.com>
2022-07-29 17:01:17 +0100

    done with side nav

--- client/src/component/SideNav.tsx
+++ client/src/component/SideNav.tsx
@@ -1,20 +1,26 @@
 import React, { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 import HomeContext, { IHomeContext } from "../context/HomeContext";
 import style from "../styles/sidenav.module.css";
 import { IUser } from "../types/user.type";
 import Home from "./Home";
 import NewCat from "./NewCat";
+import defaultUserProfile from "../defaultPic.svg";
 
 import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
 import PlaylistAddRoundedIcon from "@mui/icons-material/PlaylistAddRounded";
 import LogoutRoundedIcon from "@mui/icons-material/LogoutRounded";
 import { Button } from "@mui/material";
+import axios from "axios";
 
 const SideNav = () => {
+  const navigate = useNavigate();
+
   const { setComponent } = useContext(HomeContext) as IHomeContext;
   const [homeActive, setHomeActive] = useState(true);
   const [createActive, setCreateActive] = useState(false);
-  
+
   const data = localStorage.getItem("userInfo");
   const user: IUser = data && JSON.parse(data);
 
@@ -30,10 +36,30 @@ const SideNav = () => {
     setComponent && setComponent(<Home />);
   };
 
+  const handleLogout = async () => {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${user.access}`,
+      },
+    };
+
+    const token = {
+      refresh_token: `${user.refresh}`,
+    };
+    await axios
+      .post("/api/user/logout", token, config)
+      .then(() => {
+        localStorage.removeItem("userInfo");
+        navigate("/");
+      })
+      .catch(() => console.log("bad request"));
+  };
+
   return (
     <section className={style.body}>
       <div className={style.imageBox}>
-        <img src={user.avatar} alt="" />
+        <img src={user.avatar ? user.avatar : defaultUserProfile} alt="" />{" "}
+        <br />
         {user.first_name} {user.last_name}
       </div>
 
@@ -60,6 +86,7 @@ const SideNav = () => {
           variant="outlined"
           color="secondary"
           startIcon={<LogoutRoundedIcon />}
+          onClick={handleLogout}
         >
           Logout
         </Button>
