d563a9f48d22a037102714b94fb338e77e1ef9f8
eddybruv <edwinajong@gmail.com>
2022-07-29 16:58:46 +0100

    implemented google oauth for sign up

--- client/src/pages/auth/Login.tsx
+++ client/src/pages/auth/Login.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, useState } from "react";
+import React, { ChangeEvent, useState, useEffect } from "react";
 import CustomInput from "../../component/CustomInput";
 import style from "../../styles/login.module.css";
 import pic from "../../design.svg";
@@ -11,9 +11,14 @@ import GoogleIcon from "@mui/icons-material/Google";
 
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import { gapi } from "gapi-script";
+import { GoogleLogin } from "react-google-login";
+import Toast from "../../component/misc/Toast";
 
 const Login = () => {
   const navigate = useNavigate();
+
+  const [displayToast, setDisplayToast] = useState(false);
   const [loading, setLoading] = useState(false);
   const [user, setUser] = useState({
     email: "",
@@ -22,19 +27,44 @@ const Login = () => {
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const { value, name } = e.target;
+    setDisplayToast(false);
     setUser({
       ...user,
       [name]: value,
     });
   };
 
+  useEffect(() => {
+    const start = () => {
+      gapi.client.init({
+        clientId:
+          "26253785310-88f9ki4snsj27qmg52m01qbmoreh5d3g.apps.googleusercontent.com",
+        scope: "",
+        apiKey: "AIzaSyBqKeHwtptqPfFJ2szg_F1moJUzhJDU1Ys",
+      });
+    };
+
+    gapi.load("client:auth2", start);
+  }, []);
+
+  const responseGoogle = (response: any) => {
+    console.log(response);
+  };
+
   const handleSubmit = async () => {
+    // setDisplayToast(false)
     setLoading(() => true);
-    await axios.post("/api/user/login", user).then((data) => {
-      localStorage.setItem("userInfo", JSON.stringify(data.data));
-      setLoading(() => false);
-      navigate("/categories");
-    });
+    await axios
+      .post("/api/user/login", user)
+      .then((data) => {
+        localStorage.setItem("userInfo", JSON.stringify(data.data));
+        setLoading(() => false);
+        navigate("/categories");
+      })
+      .catch(() => {
+        setLoading(() => false);
+        setDisplayToast(true);
+      });
   };
 
   return (
@@ -45,8 +75,10 @@ const Login = () => {
             <img src={pic} alt="" />
           </div>
           <h3 className={style.slogan}>
-            WE ENABLE THE DIGITALLY INVISIBLE <br /> ECOSYSTEM WITH THE USE OF{" "}
-            <br /> DESIGN THINKING AND <br /> TECHNOLOGY
+            We provide above market remuneration for our <br /> staff, as well
+            as profit sharing options <br /> and other additional benefits to
+            make <br /> sure you`re being recognized <br /> for the value you
+            bring <br /> to your role.
           </h3>
         </div>
         <div className={style.rightContainer}>
@@ -93,19 +125,30 @@ const Login = () => {
             >
               Sign in
             </LoadingButton>
-            <LoadingButton
-              startIcon={<GoogleIcon />}
-              sx={{ mt: 2 }}
-              color="secondary"
-              fullWidth
-              variant="outlined"
-              loading={loading}
-            >
-              Sign in with Google
-            </LoadingButton>
+            <GoogleLogin
+              clientId="26253785310-88f9ki4snsj27qmg52m01qbmoreh5d3g.apps.googleusercontent.com"
+              onSuccess={responseGoogle}
+              render={(renderProps) => (
+                <LoadingButton
+                  onClick={renderProps.onClick}
+                  startIcon={<GoogleIcon />}
+                  sx={{ mt: 2 }}
+                  color="secondary"
+                  fullWidth
+                  variant="outlined"
+                  loading={loading}
+                >
+                  Continue with Google
+                </LoadingButton>
+              )}
+              cookiePolicy={"single_host_origin"}
+            />
             <p onClick={() => navigate("/register")} className={style.signup}>
               Don't have an account? Sign Up
             </p>
+            {displayToast && (
+              <Toast severity="" message="Error trying to sign in" />
+            )}
           </div>
         </div>
       </div>
