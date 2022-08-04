340676f8294ac48964b94ac70be6ced0f862a33a
eddybruv <edwinajong@gmail.com>
2022-08-04 16:22:42 +0100

    added menu

--- client/src/pages/Categories.tsx
+++ client/src/pages/Categories.tsx
@@ -1,14 +1,33 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import style from "../styles/categories.module.css";
 import SideNav from "../component/SideNav";
 import HomeContext, { IHomeContext } from "../context/HomeContext";
 
+import { IconButton } from "@mui/material";
+import MenuRoundedIcon from "@mui/icons-material/MenuRounded";
+import MenuOpenRoundedIcon from "@mui/icons-material/MenuOpenRounded";
+
 const Categories = () => {
   const { component } = useContext(HomeContext) as IHomeContext;
+  const [draw, setDraw] = useState(false);
+
+  const handleDraw = () => {
+    setDraw(!draw);
+    console.log(draw);
+  };
 
   return (
     <section className={style.body}>
-      <div className={style.sideNav}>
+      <div className={style.navButton}>
+        <IconButton onClick={handleDraw}>
+          {!draw ? (
+            <MenuRoundedIcon fontSize="large" />
+          ) : (
+            <MenuOpenRoundedIcon fontSize="large" />
+          )}
+        </IconButton>
+      </div>
+      <div className={`${style.sideNav} ${draw && style.draw}`}>
         <SideNav />
       </div>
       <div className={style.otherDiv}>{component}</div>
